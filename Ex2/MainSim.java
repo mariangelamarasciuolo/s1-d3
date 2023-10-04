965097bb38f2fe0a6528043e904b974714b1c2dd
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-10-04 16:55:47 +0200

    Esercizio 2
    iniziato esercizio 2

---
+++ Ex2/MainSim.java
@@ -0,0 +1,7 @@
+package Ex2;
+
+public class MainSim {
+    public static void main(String[] args){
+        Sim sim = new Sim ("3408965123", 10,6,5);
+    }
+}
---
+++ Ex2/Sim.java
@@ -0,0 +1,16 @@
+package Ex2;
+
+public class Sim {
+    private String numero;
+    private int credito;
+    private int durataMin;
+    private int listaNum;
+
+    public Sim (String numero, int credito, int durataMin, int listaNum) {
+    this.numero = numero;
+    this.credito = credito;
+    this.durataMin = durataMin;
+    this.listaNum = listaNum;
+    }
+    
+}
